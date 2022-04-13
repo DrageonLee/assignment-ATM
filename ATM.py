73174a39543fdcbe69cf3c9c50660dd56fc5a089
drageon lee <yg910524@gmail.com>
2022-04-14 00:16:29 +0900

    Modify : update ATM with explanation.py

--- ATM.py
+++ ATM.py
@@ -1,5 +1,6 @@
 user = []
 
+#Create user
 class Account:
     def __init__(self):
         self.name = input("What is your name : ")
@@ -11,6 +12,11 @@ class Account:
             user.append({'name' : self.name, 'PIN' : self.PIN})
         else : 
             print("Same user is enrolled")
+    '''
+    Check account of user. 
+    If there is an account, show the account that user have.
+    IF there is not and account, make the first account for user.
+    '''
     def checkAccount(self):
         self.account_list = list(i for i in filter(lambda x : x['name'] == self.name, user))
         if 'Account' not in self.account_list[0].keys():
@@ -24,6 +30,9 @@ class Account:
                 return 'Good bye'
         else :
             return self.account_list
+    '''
+    Add an account with addAccount()
+    '''
     def addAccount(self) :
         self.ID = 0
 
@@ -31,14 +40,22 @@ class Account:
             self.ID = self.account_list[0]['Account'][-1]['ID'] + 1
         self.account_list[0]['Account'].append({'ID' : self.ID, 'Balance' : 0})
         return f'New account was added. Account list : {self.account_list[0]}'
-
+'''
+Operate class if for operating ATM with accounts that has already created.
+'''
 class Operate:
     def __init__(self):
         self.user_list = user
+    '''
+    When insert Card, type the name and PIN number for checking whether the user is enrolled user or not.
+    '''
     def insertCard(self):
         name = input("Input your name : ")
         self.name = name
         user_name_list = list(i['name'] for i in self.user_list)
+        '''
+        If the user is enrolled user, go to select account.
+        '''
         if name in user_name_list:
             PIN = int(input('Please input your PIN : '))
             print(PIN)
@@ -47,24 +64,21 @@ class Operate:
                 return self.selectAccount()
             else : 
                 return 'Check PIN number again'
+#       If the user is not enrolled user, go to inserCard stage again.
         else:
             answer = input("It is not enrolled name. Do you want to retry? [Y/N] : ")
             if answer == 'Y':
                 return self.insertCard()
             else : 
                 return "Good bye"
+    #Select the accounts with number
     def selectAccount(self):
         account_list = list(i['Account'] for i in filter(lambda x : x['name'] == self.name, self.user_list))[0]
-        print(account_list)
         self.choice = int(input('Select the account : '))
 
         print(list(i['ID'] for i in account_list))
         if self.choice in list(i['ID'] for i in account_list):
-            print(1111)
-            print(list(filter(lambda x : x['ID'] == self.choice, account_list)))
             self.selected_account = list(filter(lambda x : x['ID'] == self.choice, account_list))[0]
-            print(12345)
-            print(self.selected_account)
         next = int(input("What is next? \n 1. Balance \n 2. Deposit \n 3. Withdraw \n 4. Done \n"))
         if next == 1 :
             return self.Balance()
@@ -77,9 +91,11 @@ class Operate:
         elif next == 4 :
             return 'Thank you for using'
 
+    #checek balance
     def Balance(self) : 
         return self.selected_account
     
+    #Deposit money
     def Deposit(self, money):
         name = self.name
         balance = self.selected_account
@@ -90,11 +106,13 @@ class Operate:
                     if y['ID'] == self.choice:
                         y['Balance'] = result
         return result
-
+    
+    #Withdraw money
     def Withdraw(self, money):
         name = self.name
         balance = self.selected_account
         result = balance['Balance'] - money
+        #If balance is under withdraw money, don't operate and go back.
         if result < 0:
             print("Your account has not enough money. Check again")
             print(balance)
